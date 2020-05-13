9bfa4b6a99eafb1c7da29c32c7b25bda90dfdc0a
janthenelli <57114460+janthenelli@users.noreply.github.com>
2020-05-13 11:36:27 -0400

    finished mobile layout for graphs, added hourly, daily, weekly, monthly charts to first graph, adding in social stats section

--- js/app.js
+++ js/app.js
@@ -1,37 +1,134 @@
-var ctx = document.getElementById('traffic').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: 'Traffic',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+const trafficCanvas = document.getElementById('traffic').getContext('2d')
+const trafficLIs = document.getElementsByClassName('traffic-nav-link')
+const dailyCanvas = document.getElementById('daily-traffic').getContext('2d')
+const mobileCanvas = document.getElementById('mobile-users').getContext('2d')
+
+const trafficChartData = [
+    {"points": [125, 240, 200, 170, 195, 160, 185],
+     "labels": ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]},
+    {"points": [14, 19, 28, 26, 31, 25, 22, 19],
+     "labels": ["12-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8"]},
+    {"points": [750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1750, 2250, 1750],
+     "labels": ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"]},
+    {"points": [3350, 4250, 4700, 5100, 4950, 4800, 5250, 5000, 4750, 4550, 4800, 5150],
+     "labels": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]} 
+]
+
+let trafficData = {
+    labels: trafficChartData[2].labels,
+    datasets: [{
+        data: trafficChartData[2].points,
+        backgroundColor: 'rgba(116, 119, 191, .3)',
+        borderColor: '#A9ACE5',
+        borderWidth: 2,
+        lineTension: 0,
+        pointBackgroundColor: '#FFFFFF',
+        pointBorderColor: '#7477BF',
+        pointBorderWidth: 2,
+        pointRadius: 4
+    }]
+}
+
+let trafficOptions = {
+    aspectRatio: 2.5,
+    animation: {
+        duration: 1000
+    },
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero:true
+            }
         }]
     },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
+    legend : {
+        display: false
+    }
+}
+
+let trafficChart = new Chart(trafficCanvas, {
+    type: 'line',
+    data: trafficData,
+    options: trafficOptions
+})
+
+function addData(chart, label, data) {
+    chart.data.labels = []
+    chart.data.datasets.data = []
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data = data
+    })
+    label.forEach((lab) => {
+        chart.data.labels.push(lab)
+    })
+    chart.update()
+}
+
+for (let i = 0; i < trafficLIs.length; i += 1) {
+    trafficLIs[i].addEventListener('click', function (event) {
+        const active = document.querySelector(".active");
+        active.className = event.target.className.replace(" active", "");
+        this.className += " active";
+        addData(trafficChart, trafficChartData[i].labels, trafficChartData[i].points);
+    });
+}
+
+let dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+        data: [50, 75, 150, 100, 200, 175, 75],
+        backgroundColor: '#9774bf',
+        borderColor: '#9774bf',
+        barPercentage: .5,
+    }]
+}
+
+let dailyOptions = {
+    aspectRatio: 2.5,
+    animation: {
+        duration: 1000
+    },
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero:true
+            }
+        }]
+    },
+    legend : {
+        display: false
+    }
+}
+
+let dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData,
+    options: dailyOptions
+})
+
+let mobileData = {
+    labels: ["Phones", "Tablets", "Desktop"],
+    datasets: [{
+        data: [14, 18, 68],
+        backgroundColor: ['#40b7bf', '#0fcc6e', '#9774bf'],
+        borderWidth: 0
+    }]
+}
+
+let mobileOptions = {
+    legend: {
+        position: 'right',
+        align: 'center',
+        padding: 50,
+        labels: {
+            boxWidth: 25,
+            fontStyle: 'bold'
         }
     }
-});
+}
+
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    options: mobileOptions
+})
